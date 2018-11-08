a96eb49a5b4a3b8a6c048b25aa9664e243616777
Apocrypse <lucastang1994@gmail.com>
2018-11-08 00:35:54 -0500

    bubble_sort

--- 01Introduction_to_Computation_and_C_programming/bubble_sort/main.cpp
+++ 01Introduction_to_Computation_and_C_programming/bubble_sort/main.cpp
@@ -10,7 +10,7 @@ int main() {
     }
     
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 1; j < n - 1; j++) {
+        for (int j = 1; j < n - i; j++) {
             if (a[j - 1] > a[j]) {
                 int temp = a[j];
                 a[j] = a[j - 1];
