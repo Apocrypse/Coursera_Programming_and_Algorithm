3d6ea87ea023c77c96752cd5c7bd36cf62c86c5c
Apocrypse <lucastang1994@gmail.com>
2018-11-20 23:00:06 -0500

    Week10

--- 01Introduction_to_Computation_and_C_programming/trees_and_subway/main.cpp
+++ 01Introduction_to_Computation_and_C_programming/trees_and_subway/main.cpp
@@ -6,52 +6,21 @@ int main() {
     
     int l, m;
     cin >> l >> m;
+    int r[10000] ={0};
     
-    if (m == 0) {
-        int result = l + 1;
-        cout << result << endl;
-        return 0;
-    }
-    
-    int start[100] = {0}, end[100] = {0};
-    int cur_start, cur_end;
-    cin >> cur_start >> cur_end;
-    if (cur_end < cur_start) {
-        int temp = cur_start;
-        cur_start = cur_end;
-        cur_end = temp;
-    }
-    
-    start[0] = cur_start;
-    end[0] = cur_end; 
-    int cnt = 1;
-    
-    for (int i = 1; i < m; i++) {
-        cin >> cur_start >> cur_end;
-        if (cur_end < cur_start) {
-            int temp = cur_start;
-            cur_start = cur_end;
-            cur_end = temp;
-        }
-        bool overlap = false;
-        for (int j = 0; j < cnt; j++) {
-            if ((cur_start <= end[j]) && (cur_end >= start[j])) {
-                overlap = true;
-                start[j] = min(cur_start, start[j]);
-                end[j] = max(cur_end, end[j]);
-                break;
-            }
-        }
-        if (!overlap) {
-            start[cnt] = cur_start;
-            end[cnt] = cur_end;
-            cnt++;
+    for (int i = 0; i < m; i++) {
+        int s, e;
+        cin >> s >> e;
+        for (int j = s; j <= e; j++) {
+            r[j] = 1;
         }
     }
     
-    int result = l + 1;
-    for (int i = 0; i < cnt; i++) {
-        result -= end[i] - start[i] + 1;
+    int result = 0;
+    for (int i = 0; i <= l; i++) {
+        if (r[i] == 0) {
+            result++;
+        }
     }
     cout << result << endl;
     
